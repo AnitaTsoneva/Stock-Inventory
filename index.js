b1e527bf3b39aa2af18e697cc4b6278266582451
AnitaTsoneva <tsoneva2@uni.coventry.ac.uk>
2019-11-23 22:34:44 +0000

    Added all the changes after an issue with git flow branching

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ const staticDir = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const koaBody = require('koa-body')({multipart: true, uploadDir: '.'})
 const session = require('koa-session')
+const hbs = require('koa-hbs');
+
 //const jimp = require('jimp')
 
 /* IMPORT CUSTOM MODULES */
 const User = require('./modules/user')
+const Stock = require('./modules/stock')
 
 const app = new Koa()
 const router = new Router()
@@ -47,61 +50,149 @@ router.get('/', async ctx => {
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
 	}
-})
+});
 
+/**
+ * The stock page.
+ *
+ * @name Stock Page
+ * @route {GET} /stock
+ */
+router.get('/stock_get', async ctx => {
+
+	try {
+		var response = await stock.getAllItems();		
+		//await ctx.render('home', {title: 'Favourite Books', books: data})
+	} catch(err) {
+		ctx.body = err.message
+	}
+
+	//await ctx.render('getAllItems', response)
+
+});
+
+
+/**
+ * The script to process new user registrations.
+ *
+ * @name Stock Script
+ * @route {POST} /stock
+ */
+router.post('/stock', koaBody, async ctx => {
+	try {
+
+		const body = ctx.request.body;
+		const stock = await new Stock(dbName)
+		await stock.addItem(body)
+
+		//var response = await stock.getAllItems();
+		//await ctx.render('index', response)
+
+		// redirect to the home page
+		//ctx.redirect(`/?msg=new item "${body.item}" added`)
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+}); 
+
+router.post('/stock_add', koaBody, async ctx => {
+	try {
+		console.log('Im hereeeee')
+		//const body = ctx.request.body;
+		//const stock = await new Stock(dbName)
+		//await stock.addItem(body)
+
+		//var response = await stock.getAllItems();
+		//await ctx.render('index', response)
+
+		// redirect to the home page
+		//ctx.redirect(`/?msg=new item "${body.item}" added`)
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+});
 /**
  * The user registration page.
  *
  * @name Register Page
  * @route {GET} /register
  */
-router.get('/register', async ctx => await ctx.render('register'))
+router.get('/register', async ctx => await ctx.render('register'));
 
 /**
  * The script to process new user registrations.
  *
  * @name Register Script
  * @route {POST} /register
+ * @param {string} user - The user's name.
+ * @param {string} pass - The user's password.
+ * @param {string} department - The user's department.
+ * 
  */
 router.post('/register', koaBody, async ctx => {
 	try {
 		// extract the data from the request
 		const body = ctx.request.body
-		console.log(body)
 		// call the functions in the module
 		const user = await new User(dbName)
-		await user.register(body.user, body.pass)
+		await user.register(body)
 		// await user.uploadPicture(path, type)
 		// redirect to the home page
 		ctx.redirect(`/?msg=new user "${body.name}" added`)
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
 	}
-})
+});
 
+/**
+ * The script to process user login.
+ *
+ * @name Login Script
+ * @route {GET} /login
+ * @param {string} department - The user's department.
+ * 
+ */
 router.get('/login', async ctx => {
 	const data = {}
 	if(ctx.query.msg) data.msg = ctx.query.msg
 	if(ctx.query.user) data.user = ctx.query.user
 	await ctx.render('login', data)
-})
+});
 
+/**
+ * The script to process new user registrations.
+ *
+ * @name Login Script
+ * @route {POST} /login
+ * @param {string} user - The user's name.
+ * @param {string} pass - The user's password.
+ * @param {string} department - The user's department.
+ * 
+ */
 router.post('/login', async ctx => {
 	try {
-		const body = ctx.request.body
-		const user = await new User(dbName)
-		await user.login(body.user, body.pass)
-		ctx.session.authorised = true
-		return ctx.redirect('/?msg=you are now logged in...')
+		const body = ctx.request.body;
+		const user = await new User(dbName);
+		await user.login(body.user, body.pass);
+		ctx.session.authorised = true;
+	
+		const stock = await new Stock(dbName)
+		var items = await stock.getAllItems();
+
+		console.log(body);
+		console.log('***************')
+		await ctx.render('index', {username: body.user, books: items})
+		
+		//return ctx.redirect('/?msg=you are now logged in...')
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
 	}
-})
+});
 
 router.get('/logout', async ctx => {
 	ctx.session.authorised = null
 	ctx.redirect('/?msg=you are now logged out')
-})
+});
 
 app.use(router.routes())
 module.exports = app.listen(port, async() => console.log(`listening on port ${port}`))
---
+++ modules/stock.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const sqlite = require('sqlite-async');
+
+
+module.exports = class Stock {
+
+    constructor(dbName = ':memory:') {
+		return (async() => {
+			
+			this.db = await sqlite.open(dbName)
+
+			//const sql1 = 'DROP TABLE stock ;'
+			//await this.db.run(sql1);
+			// we need this table to store the user accounts
+			const sql = 'CREATE TABLE IF NOT EXISTS stock (id INTEGER PRIMARY KEY AUTOINCREMENT, ena_num VAR, itemName TEXT, quantity INTEGER);'
+			await this.db.run(sql)
+			return this
+		})()
+	}
+
+
+	/**
+	 *  Checking if items exsist in the dabase, if not addimg more.
+	 *  Increasing the quantity of items if they already exist.
+	*/
+    async addItem(itemValues) {
+
+        try {
+			let sql = `SELECT COUNT(id) as records FROM stock WHERE ena_num="${itemValues.ena_num}";`
+			const data = await this.db.get(sql)
+
+			// Check if item exist
+			if(data.records !== 0) {
+				let sql = `SELECT quantity FROM stock WHERE ena_num="${itemValues.ena_num}";`
+				const existingQuantity = await this.db.get(sql)
+				let newQuantity = Number(itemValues.quantity) + existingQuantity.quantity;
+
+				sql = `UPDATE stock SET quantity = "${newQuantity}" WHERE ena_num="${itemValues.ena_num}";`
+				await this.db.get(sql)
+				return true
+
+			}else{
+				sql = `INSERT INTO stock(ena_num, itemName, quantity) VALUES("${itemValues.ena_num}", "${itemValues.itemName}" ,"${itemValues.quantity}")`
+				await this.db.run(sql)
+				return true
+			}
+						
+		} catch(err) {
+            console.log(err)
+			throw err
+        }
+        
+    }
+
+	/**
+	 *  Retrieveing all items in the database
+	*/
+    async getAllItems() {
+
+        try {
+			let sql = `SELECT * FROM stock;`;
+			const data = await this.db.all(sql)
+			//if(data.records !== 0) throw new Error(`Item Name "${ena_num}" already exists`)
+
+			//sql = `INSERT INTO stock(ena_num, quantity) VALUES("${ena_num}", "${quantity}")`
+			//await this.db.run(sql)
+			return data;
+		} catch(err) {
+            console.log(err)
+			throw err
+        }
+	}
+	
+	async getItemBy() {
+
+        // TODO 
+    }
+
+    
+
+}
